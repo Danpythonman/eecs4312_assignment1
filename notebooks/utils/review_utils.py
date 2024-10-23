a85f660824c513a91cd54cdff91517190cdc9bcd
Daniel Di Giovanni <dannyjdigio@gmail.com>
2024-10-22 22:51:51 -0400

    Moved find reviews by keyword functions to utils package, added better documentation.

--- notebooks/utils/review_utils.py
+++ notebooks/utils/review_utils.py
@@ -2,6 +2,9 @@ from google_play_scraper import app, reviews_all
 import pickle
 import os
 from pathlib import Path
+from datetime import datetime
+import pandas as pd
+from typing import List
 
 
 # Path of this file
@@ -20,21 +23,21 @@ DATA_DIRECTORY_PATH = NOTEBOOK_DIRECTORY_PATH.parent / 'data'
 DATA_DIRECTORY_PATH.mkdir(exist_ok=True)
 
 
-def CACHE_FILE(app_id):
+def CACHE_FILE(app_id: str) -> Path:
     """
     Get the path of the pickle file for the reviews of the given `app_id`.
     """
     return DATA_DIRECTORY_PATH / f'data_cache-{app_id}.pkl'
 
 
-def get_app_reviews(app_id):
+def get_app_reviews(app_id: str) -> list:
     """
     Fetch reviews for an app.
     """
     return reviews_all(app_id, lang='en', country='us', sleep_milliseconds=0)
 
 
-def get_google_play_data(app_id, flush=False):
+def get_google_play_data(app_id: str, flush: bool=False) -> list:
     """
     Fetches reviews from the Google Play Store for the given `app_id`, caching
     them once retrieved.
@@ -66,3 +69,48 @@ def get_google_play_data(app_id, flush=False):
         print('Data fetched from the web and cached.')
 
     return data
+
+
+def find_reviews_by_keyword_list(df: pd.DataFrame, keyword_list: List[str]) -> None:
+    """
+    Prints all reviews in `df` (a pandas DataFrame) whose `'content'` attribute
+    contains any of the keywords in `keyword_list` as a substring.
+
+    Also prints the number of reviews that were found, including a breakdown of
+    the number of reviews from each topic.
+    """
+    total = 0
+    topic_total_map = {}
+
+    for i in range(df.shape[0]):
+        review = df.iloc[i]
+        if any(keyword in review['content'].lower() for keyword in keyword_list):
+            total += 1
+
+            topic = review['topic']
+            if topic in topic_total_map:
+                topic_total_map[topic] += 1
+            else:
+                topic_total_map[topic] = 1
+
+            date = datetime.strptime(review['at'], '%Y-%m-%d %H:%M:%S')
+
+            print(f"TOPIC {topic} [{date.strftime('%Y-%m-%d')}]: {review['content']}")
+
+    print('===== Totals from each topic =====')
+
+    for topic in sorted(topic_total_map.keys()):
+        print(f'Topic {topic}: {topic_total_map[topic]}', end=', ')
+
+    print(f'Total: {total}, all else 0')
+
+
+def find_reviews_by_keyword(df: pd.DateOffset, keyword: str) -> None:
+    """
+    Prints all reviews in `df` (a pandas DataFrame) whose `'content'` attribute
+    contains `keyword` as a substring.
+
+    Also prints the number of reviews that were found, including a breakdown of
+    the number of reviews from each topic.
+    """
+    find_reviews_by_keyword_list(df, [keyword])
